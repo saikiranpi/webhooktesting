c0a742a3bfd0f2463fd44e7d524e98d8fa9d361d
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-04-14 12:04:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION=$(date +%H-%M-%S)
-docker build -t sreeharshav/testingbuild:${VERSION} .
-docker push sreeharshav/testingbuild:${VERSION}
+docker build -t kiran2361993/testingbuild:${VERSION} .
+docker push kiran2361993/testingbuild:${VERSION}
 docker -H tcp://10.1.1.200:2375 stop nginx
-docker -H tcp://10.1.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx sreeharshav/testingbuild:${VERSION}
+docker -H tcp://10.1.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx kiran2361993/testingbuild:${VERSION}
 echo "---------------------"
